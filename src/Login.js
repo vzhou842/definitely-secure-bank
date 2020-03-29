1a333ff988332d917108ce56b6bbaf09f03fe65c
Victor Zhou <victorzhou@fb.com>
2020-03-29 15:12:30 -0700

    Setup Login skeleton

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export default function Login() {
-  return <div />;
+  return (
+    <form action="/login">
+      <label htmlFor="username">Username:</label>
+      <input type="text" id="username" />
+      <br />
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" />
+      <br />
+      <input type="submit" />
+    </form>
+  );
 }
