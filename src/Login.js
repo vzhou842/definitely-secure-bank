e1269aa29a05250774d31b74038d7a7c3f92bb3d
Victor Zhou <victorzhou@fb.com>
2020-04-12 18:51:19 -0700

    Migrate login form to react-bootstrap

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export default function Login({ user }) {
   const history = useHistory();
@@ -12,14 +14,16 @@ export default function Login({ user }) {
   }, [history, user]);
 
   return (
-    <form method="POST" action="/login">
-      <label htmlFor="username">Username:</label>
-      <input type="text" name="username" id="username" placeholder="Username" />
-      <br />
-      <label htmlFor="password">Password:</label>
-      <input type="password" name="password" id="password" placeholder="Password" />
-      <br />
-      <input type="submit" />
-    </form>
+    <Form method="POST" action="/login">
+      <Form.Group>
+        <Form.Label>Username:</Form.Label>
+        <Form.Control type="text" name="username" placeholder="Username" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label htmlFor="password">Password:</Form.Label>
+        <Form.Control type="password" name="password" placeholder="Password" />
+      </Form.Group>
+      <Button type="submit">Submit</Button>
+    </Form>
   );
 }
