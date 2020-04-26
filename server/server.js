9e2e40c6af7f59cbfccc897d69492b31720c750b
Victor Zhou <victorzhou@fb.com>
2020-04-25 19:29:27 -0700

    Add support for GET on other paths

--- server/server.js
+++ server/server.js
@@ -15,13 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  if (username == null || password == null || username.length < 1 || password.length < 1) {
-    return res.status(400).end();
-  }
-  const sessionID = db.handleLogin(username, password);
-  res.cookie('session', sessionID, { maxAge: ONE_HOUR_MS }).redirect('/');
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(BUILD_PATH, 'index.html'));
 });
 
 app.get('/user', (req, res) => {
@@ -32,6 +27,19 @@ app.get('/user', (req, res) => {
   } else {
     res.status(200).json(user);
   }
+});
+
+app.get('*', (req, res) => {
+  res.redirect('/');
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  if (username == null || password == null || username.length < 1 || password.length < 1) {
+    return res.status(400).end();
+  }
+  const sessionID = db.handleLogin(username, password);
+  res.cookie('session', sessionID, { maxAge: ONE_HOUR_MS }).redirect('/');
 });
 
 // Auth middleware
